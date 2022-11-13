121203f4abca52a79ed196e207c6b9ec58933f3a
Hector Triana <ptb1204@hotmail.com>
2022-11-13 00:34:09 -0500

    credential dict printing, redis connection & mongo cluster credentials

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import json
 from packages.http_request.httpmanager import HttpManager
+from packages.redis.redismanager import RedisManager
 
-httpManager = HttpManager()
-httpManager.fetch_init_data()
+def main():
+    http_manager = HttpManager()
+    redis_manager = RedisManager()
+    
+    redis_credentials = http_manager.fetch_init_data()
+    print('redis_credentials: ', json.dumps(redis_credentials['credentials'], indent=4))
+    
+    redis_manager.redis_connection_pool(redis_credentials['credentials'])
 
+main()
--- packages/http_request/httpmanager.py
+++ packages/http_request/httpmanager.py
@@ -6,14 +6,16 @@ class HttpManager:
     def __init__(self):
         self.__urlManager = URLManager()
         
-        
     
     def fetch_init_data(self):
-        load={}
-        receive = requests.post(self.__urlManager.get_Url_Endpoint(), data=load)
-        print(receive.json())
-        print('________')
-        credentials = receive.json()
-        print(credentials['credentials'])
+        credentials = {}
+        try:
+            receive = requests.post(self.__urlManager.get_Url_Endpoint(), {})
+            credentials = receive.json()
+            return credentials
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        
+            
         
 
